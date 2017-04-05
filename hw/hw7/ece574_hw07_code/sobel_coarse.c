a7d7a338909e59bd004451d2cc89b9bd7a886fe5
Qihao He <qi.he@maine.edu>
2017-04-05 18:47:42 -0400

    debug for negative count

--- hw/hw7/ece574_hw07_code/sobel_coarse.c
+++ hw/hw7/ece574_hw07_code/sobel_coarse.c
@@ -316,7 +316,8 @@ int main(int argc, char **argv) {
 	}
 	/* other processes also has the arraysize_image */
 	arraysize_image=image.x*image.y*image.depth*sizeof(char);
-	printf("The arraysize_image is:%lu\n",arraysize_image);
+	/* debug for negative count */
+	printf("R%dThe arraysize_image is:%lu\n",rank,arraysize_image);
 
 	/* Use MPI_Bcast() to broadcast the entire image data from rank0 to all the
 	other ranks. You want to broadcast “image.pixels”, not all of image (remember,
