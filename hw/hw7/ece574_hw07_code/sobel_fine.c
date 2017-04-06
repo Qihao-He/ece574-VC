f98d8b798da80645a96377eb917a6286564066fe
Qihao He <qi.he@maine.edu>
2017-04-06 18:05:01 -0400

    cp sobel_coarse.c to sobel_fine.c

--- hw/hw7/ece574_hw07_code/sobel_fine.c
+++ hw/hw7/ece574_hw07_code/sobel_fine.c
@@ -11,6 +11,7 @@
 #include <jpeglib.h>
 
 #include <mpi.h>
+#define ARRAYSIZE 3//image.x, image.y,image.depth are the 3 element of the array
 
 /* Filters */
 static int sobel_x_filter[3][3]={{-1,0,+1},{-2,0,+2},{-1,0,+1}};
@@ -32,6 +33,11 @@ struct convolve_data_t {
 	int yend;
 };
 
+/* Modify generic_convolve so it takes a range of y to operate on. Then
+calculate this y range based on the rank and size parameters. MPI_Gather will
+gather from the *start* of each buffer and put it in the proper place in the
+results. So you hanve to modify the convolve routine to store the output
+starting at offset 0, rather than at offset ystart. */
 
 /* very inefficient convolve code */
 static void *generic_convolve(void *argument) {
@@ -58,7 +64,7 @@ static void *generic_convolve(void *argument) {
 	depth=old->depth;
 	width=old->x*old->depth;
 
-	if (ystart==0) ystart=1;
+	if (ystart==0) ystart=1;// edge problem
 	if (yend==old->y) yend=old->y-1;
 
 	for(d=0;d<3;d++) {
@@ -76,8 +82,18 @@ static void *generic_convolve(void *argument) {
 		if (sum>255) sum=255;
 
 		new->pixels[(y*width)+x*depth+d]=sum;
+
 	     }
 	   }
+	}
+	/* Moving the range from ystart to yend to offset 0. */
+	for(d=0;d<3;d++) {
+		 for(x=1;x<old->x-1;x++) {
+			 for(y=ystart;y<yend;y++) {
+				 new->pixels[((y-ystart)*width)+x*depth+d]=
+				 new->pixels[(y*width)+x*depth+d];
+			 }
+		 }
 	}
 
 	return NULL;
@@ -225,8 +241,14 @@ int main(int argc, char **argv) {
 
 	struct image_t image,sobel_x,sobel_y,new_image;
 	struct convolve_data_t sobel_data[2];
-	double start_time,load_time,store_time,convolve_time,combine_time;
-	int result;
+	double start_time,load_time=0,store_time,convolve_time=0,combine_time;
+	int result,i;
+	int A[ARRAYSIZE]; //A Buffer
+	int numtasks,rank;//# of tasks, rank index
+	MPI_Status Stat;
+	unsigned long int arraysize_image; //arraysize of the image
+	unsigned char *gather_sobel_x=NULL;//receive buffer for soble_x
+	unsigned char *gather_sobel_y=NULL;//receive buffer for soble_y
 
 	/* Check command line usage */
 	if (argc<2) {
@@ -241,12 +263,78 @@ int main(int argc, char **argv) {
 		MPI_Abort(MPI_COMM_WORLD, result);
 	}
 
+	/* Calls MPI_wtime() to get the wallclock times for Load, Convolve, Combine,
+	and Store like we did with PAPI in the OpenMP code. You only need to record
+	and print these from rank 0. */
 	start_time=MPI_Wtime();
 
-	/* Load an image */
-	load_jpeg(argv[1],&image);
+	/* Get number of tasks and our process number (rank) */
+	MPI_Comm_size(MPI_COMM_WORLD,&numtasks);//number os tasks
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);//process number(rank)
 
-	load_time=MPI_Wtime();
+	printf("R%d: Number of tasks= %d My rank= %d\n",
+		rank,numtasks,rank);
+
+	/* Only load the jpeg in rank 0 */
+	if (rank==0) {
+		/* Initialize Array */
+		printf("R0: Initializing array\n");
+		/* Load an image */
+		load_jpeg(argv[1],&image);
+
+		/* Send the image parameters (image.x, image.y, image.depth) to all other
+		ranks, sending an array of 3 INTS */
+		A[0]= image.x;
+		A[1]= image.y;
+		A[2]= image.depth;
+
+		for(i=1;i<numtasks;i++) {
+			printf("R0: Sending %d ints to Rank%d\n",
+				ARRAYSIZE,i);
+			result = MPI_Send(A,/* buffer */
+					ARRAYSIZE,			/* count */
+					MPI_INT,				/* type */
+					i,							/* destination */
+					13,							/* tag */
+					MPI_COMM_WORLD);
+		}
+		load_time=MPI_Wtime();
+
+		/* In Rank 0, get the size of the image */
+		arraysize_image=image.x*image.y*image.depth*sizeof(char);
+
+		gather_sobel_x=malloc(arraysize_image);//dynamically allocate Memory
+		gather_sobel_y=malloc(arraysize_image);//dynamically allocate Memory
+	}
+	else {
+		/* MPI_Recv is required for other processes */
+		/* This is run by all the non-master processes */
+		result = MPI_Recv(A,/* buffer */
+				ARRAYSIZE,			/* count */
+				MPI_INT,				/* type */
+				0,							/* source */
+				13,							/* tag */
+				MPI_COMM_WORLD,	/* communicator */
+				&Stat);					/* status */
+
+		image.x=A[0];
+		image.y=A[1];
+		image.depth=A[2];
+		arraysize_image=image.x*image.y*image.depth*sizeof(char);
+
+		/* Malloc image.pixels input image in the non rank-0 threads */
+		printf("MALLOC R%d\n",rank);
+		image.pixels=malloc(arraysize_image);
+	}
+
+	/* Use MPI_Bcast() to broadcast the entire image data from rank0 to all the
+	other ranks. You want to broadcast “image.pixels”, not all of image (remember,
+ 	MPI you can’t send structs, just arrays). */
+	MPI_Bcast(image.pixels,	/* buffer */
+		arraysize_image,			/* count */
+		MPI_CHAR,							/* type */
+		0,										/* root source */
+		MPI_COMM_WORLD);
 
 	/* Allocate space for output image */
 	new_image.x=image.x;
@@ -266,40 +354,64 @@ int main(int argc, char **argv) {
 	sobel_y.depth=image.depth;
 	sobel_y.pixels=malloc(image.x*image.y*image.depth*sizeof(char));
 
+	/* Calculate this y range based on the rank and size parameters. */
+	/* Each rank should work on part of the image ranging form ystart to yend. */
 	/* convolution */
 	sobel_data[0].old=&image;
 	sobel_data[0].new=&sobel_x;
 	sobel_data[0].filter=&sobel_x_filter;
-	sobel_data[0].ystart=0;
-	sobel_data[0].yend=image.y;
+	sobel_data[0].ystart=rank*image.y/numtasks;//rank*(size_img)
+	sobel_data[0].yend=(rank+1)*image.y/numtasks;//(rank+1)*(size_img)/numtasks
 	generic_convolve((void *)&sobel_data[0]);
 
 	sobel_data[1].old=&image;
 	sobel_data[1].new=&sobel_y;
 	sobel_data[1].filter=&sobel_y_filter;
-	sobel_data[1].ystart=0;
-	sobel_data[1].yend=image.y;
+	sobel_data[1].ystart=rank*image.y/numtasks;//rank*(size_img)
+	sobel_data[1].yend=(rank+1)*image.y/numtasks;//(rank+1)*(size_img)/numtasks
 	generic_convolve((void *)&sobel_data[1]);
+
+/* Use MPI_Gather() to gather results in rank 0 */
+MPI_Gather(sobel_x.pixels,	/* send buffer */
+	arraysize_image/numtasks,	/* count */
+	MPI_CHAR,									/* type */
+	gather_sobel_x,						/* receive buffer */
+	arraysize_image/numtasks,	/* count */
+	MPI_CHAR,									/* type */
+	0,												/* root source */
+	MPI_COMM_WORLD);
+
+MPI_Gather(sobel_y.pixels,	/* send buffer */
+	arraysize_image/numtasks,	/* count */
+	MPI_CHAR,									/* type */
+	gather_sobel_y,						/* receive buffer */
+	arraysize_image/numtasks,	/* count */
+	MPI_CHAR,									/* type */
+	0,												/* root source */
+	MPI_COMM_WORLD);
+
+if (rank==0){
+	/* Put array gather_sobel_x, gather_sobel_y in structure sobel_x, sobel_y */
+	sobel_x.pixels=gather_sobel_x;
+	sobel_y.pixels=gather_sobel_y;
 
 	convolve_time=MPI_Wtime();
 
-	/* Combine to form output */
+	/* On rank 0 alone, run combine to form output */
 	combine(&sobel_x,&sobel_y,&new_image);
-
 	combine_time=MPI_Wtime();
 
-	/* Write data back out to disk */
+	/* On rank 0 alone, write the output to a file */
 	store_jpeg("out.jpg",&new_image);
-
 	store_time=MPI_Wtime();
 
 	printf("Load time: %lf\n",load_time-start_time);
-        printf("Convolve time: %lf\n",convolve_time-load_time);
-        printf("Combine time: %lf\n",combine_time-convolve_time);
-        printf("Store time: %lf\n",store_time-combine_time);
+	printf("Convolve time: %lf\n",convolve_time-load_time);
+	printf("Combine time: %lf\n",combine_time-convolve_time);
+	printf("Store time: %lf\n",store_time-combine_time);
 	printf("Total time = %lf\n",store_time-start_time);
-
-	MPI_Finalize();
-
-	return 0;
+}
+/* MPI_Finalize at the end */
+MPI_Finalize();
+return 0;
 }
