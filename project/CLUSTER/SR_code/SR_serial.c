963b3e37487eb60d645f22a4066488be1fae665a
Qihao He <qi.he@maine.edu>
2017-05-01 11:14:31 -0400

    update

--- project/CLUSTER/SR_code/SR_serial.c
+++ project/CLUSTER/SR_code/SR_serial.c
@@ -22,6 +22,6 @@ int main(int argc, char *argv[]) {
 	area=f(0)-f(n);
 	for(i=1;i<=n/2;i++) area+=4.0*f(2*i-1)+2*f(2*i);
 	area/=(3.0*n);
-	printf("Approximation of pi: %13.11f\n",area);
+	printf("Approximation of pi: %f\n",area);
 	return 0;
 }
