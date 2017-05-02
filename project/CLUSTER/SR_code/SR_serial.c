d87177e8cade3f4038f4b03d1801bf0cf8eddbd3
Qihao He <qi.he@maine.edu>
2017-05-01 21:31:30 -0400

    update

--- project/CLUSTER/SR_code/SR_serial.c
+++ project/CLUSTER/SR_code/SR_serial.c
@@ -15,7 +15,7 @@
 //64 data points after decimal point
 #define truepivalue 3.1415926535897932384626433832795028841971693993751058209749445923
 
-double f(double i){
+double f(int i){
 	double x;
 	x=(double)i/(double)n;
 	return 4.0/(1.0+x*x);
@@ -23,7 +23,7 @@ double f(double i){
 
 int main(int argc, char *argv[]) {
 	double area;
-	double i;
+	int i;
 	double Rerror;//Relative error
 	double error;
 
