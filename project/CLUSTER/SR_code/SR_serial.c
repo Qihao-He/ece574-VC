99a0cc6c37460e05c7264bc0cc1b7bce83d6e8c6
Qihao He <qi.he@maine.edu>
2017-05-02 10:23:14 -0400

    update

--- project/CLUSTER/SR_code/SR_serial.c
+++ project/CLUSTER/SR_code/SR_serial.c
@@ -9,11 +9,12 @@
 #include <math.h>
 
 #define n 1e8 //iteration times
-#define epsilon 2.220446e-16//epsilon
-
+// #define epsilon 2.220446e-16//epsilon
+#define epsilon 1e-17//epsilon
 /* True value of Pi from Wolfwww.wolframalpha.com */
 //64 data points after decimal point
 // #define truepivalue 3.1415926535897932384626433832795028841971693993751058209749445923
+#define truepivalue 3.14159265358979323
 
 double f(int i){
 	double x;
@@ -26,20 +27,21 @@ int main(int argc, char *argv[]) {
 	int i;
 	double Rerror;//Relative error
 	double error;
-	long double truepivalue=3.1415926535897932384626433832795028841971693993751058209749445923;
+	// long double truepivalue=3.141592653589793;
 
 	area=f(0)-f(n);
 	for(i=1;i<=n/2;i++) area+=4.0*f(2*i-1)+2*f(2*i);
 	area/=(3.0*n);
 
-	printf("Appr pi: %13.64f\n",area);//print Approximate pi
-	printf("True pi: %13.64Lf\n",truepivalue);//true value of pi
+	printf("Appr pi: %13.17f\n",area);//print Approximate pi
+	printf("True pi: %13.17f\n",truepivalue);//true value of pi
+	printf("Epsilon: %1.17f\n",epsilon);//epsilon
 
 	/* Relative error in epsilon */
 	// printf("epsilon is: %f\n",(double)epsilon);
 	Rerror=abs(area-(double)truepivalue)/(double)epsilon;
 	error=abs(area-(double)truepivalue);
-	printf("Relative error in epsilon is: %13.64f\n",Rerror);
-	printf("error is: %13.64f\n",error);
+	printf("Relative error in epsilon is: %13.17f\n",Rerror);
+	printf("error is: %13.17f\n",error);
 	return 0;
 }
